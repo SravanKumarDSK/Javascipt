8963f641805b256e51fe40a442146a3fce8294b9
SravanKumarDSK <sravankumardasari04@gmail.com>
2024-10-26 10:00:11 +0530

    added

---
+++ functions/evenandnumber.js
@@ -0,0 +1,12 @@
+function checkEvenOrAdd(number){
+    if(number%2==0){
+        console.log(number,"is even");
+    }
+    else{
+        console.log(number,"is add");
+    }
+}
+checkEvenOrAdd(1);
+checkEvenOrAdd(3);
+checkEvenOrAdd(5);
+checkEvenOrAdd(2);
