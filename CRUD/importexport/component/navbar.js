cbb25fcc25c2d51854bf11b5b3d159ce21595766
SravanKumarDSK <sravankumardasari04@gmail.com>
2024-11-01 11:31:26 +0530

    added

---
+++ CRUD/importexport/component/navbar.js
@@ -0,0 +1,11 @@
+function navbar(){
+    return `
+    <div id="navbar_import">
+        <h1>HOME</h1>
+        <div id="options">
+            <h3><a href="./cart.html">Cart</a></h3>
+            <h3><a href="./payment.html">Payment</a></h3>
+        </div>
+    </div>`;
+}
+export default navbar;
---
+++ CRUD/index.js
@@ -0,0 +1,6 @@
+let isEven=require('is-even');
+
+console.log(isEven(3));
+
+let isOdd=require('is-odd');
+console.log(isOdd(18));
